e98eb059edcc5b0eb307d520a33b1057c764cfc4
Alexander Vyzhymov <vizimov@gmail.com>
2023-07-22 17:48:52 +0300

    created noticesSearch markup

--- src/pages/NoticesPage/NoticesPage.styled.js
+++ src/pages/NoticesPage/NoticesPage.styled.js
@@ -1,6 +1,26 @@
 import styled from 'styled-components';
 
 export const NoticesPageContainer = styled('div')`
+  width: 100%;
+  height: 100%;
+  padding-top: ${({ theme }) => theme.space[3] * 5 + 'px'};
+
+  @media screen and (min-width: ${({ theme }) => theme.breakpoints.tablet}) {
+    padding-top: ${({ theme }) => theme.space[4] * 5 + 'px'};
+  }
+`;
+
+export const Title = styled.h2`
+  font-family: ${({ theme }) => theme.fonts.main.bold};
+  font-size: ${({ theme }) => theme.fontSizes[4]};
+  text-align: center;
+
+  @media screen and (min-width: ${({ theme }) => theme.breakpoints.tablet}) {
+    font-size: ${({ theme }) => theme.fontSizes[7]};
+  }
+`;
+
+export const NoticeFilterContainer = styled.div`
   display: flex;
   justify-content: center;
   align-items: flex-start;
