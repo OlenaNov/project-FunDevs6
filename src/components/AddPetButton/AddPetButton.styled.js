070b90b934d760de630a1f0ece99c2efc8e6179a
Olena Novitska <109989618+OlenaNov@users.noreply.github.com>
2023-07-26 10:07:59 +0200

    make commit add pet

--- src/components/AddPetButton/AddPetButton.styled.js
+++ src/components/AddPetButton/AddPetButton.styled.js
@@ -1,4 +1,7 @@
 import styled from '@emotion/styled';
+import { theme } from 'theme/theme';
+const { fontSizes, colors, breakpoints } = theme;
+
 
 export const Btn = styled.button`
   position: fixed;
@@ -16,9 +19,15 @@ export const Btn = styled.button`
   border-radius: 50px;
   border: none;
   text-decoration: none;
+  background: #54adff;
+  box-shadow: 3px 8px 14px rgba(136, 198, 253, 0.19);
+  font-family: 'Manrope', sans-serif;
+  font-weight: 'Manrope-SemiBold';
+  font-size: ${fontSizes[0]}px;
 
   line-height: 16px;
 
+  color: #FFFFFF;
   cursor: pointer;
 
   svg {
@@ -27,19 +36,22 @@ export const Btn = styled.button`
   }
 
   :hover {
+  background: ${colors.gradientBlue};
   }
 
-  @media screen and () {
+  @media screen and (${breakpoints.tablet}) {
     position: static;
     margin-top: 60px;
     margin-left: auto;
     gap: 8px;
     flex-direction: row;
     width: 130px;
-
+    background: '#54ADFF';
     border-radius: 40px;
     height: 40px;
 
+  font-weight: 'Manrope-Bold';
+    font-size: ${fontSizes[2]}px;
     line-height: 22px;
   }
 `;
---
+++ src/components/MoreInfo/MoreInfo.styled.js
@@ -0,0 +1,38 @@
+import styled from '@emotion/styled';
+
+export const Thumb = styled.div`
+  display: flex;
+  flex-direction: row;
+  gap: 24px;
+  @media (max-width: 767px) {
+    flex-direction: column;
+    gap: 0;
+  }
+`;
+
+export const PlusFileButton = styled.button`
+  width: 114px;
+  height: 114px;
+  background: #cce4fb;
+  border: none;
+  border-radius: 20px;
+  padding: 0px;
+  margin-bottom: 7px;
+  @media (min-width: 768px) {
+    width: 182px;
+    height: 182px;
+    border-radius: 40px;
+  }
+`;
+
+export const Image = styled.img`
+  object-fit: cover;
+  object-position: top center;
+  width: 100%;
+  height: 100%;
+  border: none;
+  border-radius: 20px;
+  @media (min-width: 768px) {
+    border-radius: 40px;
+  }
+`;
