7ee03b8f3e46a358e06c9b75f0538ed9daa108fe
Olena Novitska <109989618+OlenaNov@users.noreply.github.com>
2023-07-28 06:42:54 +0200

    fix workflow

--- src/components/NoticesCategoriesList/NoticesCategoriesList.jsx
+++ src/components/NoticesCategoriesList/NoticesCategoriesList.jsx
@@ -1,4 +1,4 @@
-import React, { useState,useEffect } from 'react';
+import React, { useState } from 'react';
 import NoticeCategoryItem from '../../components/NoticeCategoryItem/NoticeCategoryItem';
 import { CategoriesListWrapper } from './NoticesCategoriesList.styled';
 import Pagination from '../Pagination/Pagination';
