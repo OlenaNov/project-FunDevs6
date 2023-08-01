28b468154da59f52ecc69bc2e4868d474314b1b3
Olena Novitska <109989618+OlenaNov@users.noreply.github.com>
2023-08-01 21:33:01 +0200

    make header logout

--- src/components/Layout/Layout.jsx
+++ src/components/Layout/Layout.jsx
@@ -5,13 +5,37 @@ import { Main } from './Layout.styled';
 import Loader from 'components/Loader';
 import NavBar from 'components/NavBar/NavBar';
 
+import { logout } from 'redux/auth/auth-operations';
+import Modal from 'components/Modal/Modal';
+import { useDispatch } from 'react-redux';
+import { useState } from 'react';
+import ModalContent from 'components/ModalContent/ModalContent';
+
 export const Layout = () => {
+  
+  const dispatch = useDispatch();
+
+  const [showModal, setShowModal] = useState(false);
+
+  const modalYes = () => {
+    dispatch(logout());
+    setShowModal(false);
+  };
+
+  const toggleModal = () => {
+    setShowModal(s => !s);
+  };
+
   return (
     <>
       <Main>
-        <NavBar />
+        <NavBar onOpen={() => setShowModal(true)} />
         <Suspense fallback={<Loader />}>
           <Outlet />
+          {showModal && 
+        <Modal 
+          onClose={toggleModal} 
+          children={() => <ModalContent title="Already leaving?" fnYes={() => modalYes()} fnCancel={() => setShowModal(false)} /> } />}
         </Suspense>
       </Main>
     </>
--- src/components/NavBar/Other/LogOutCopmponents/MobileLogout/MobLogout.jsx
+++ src/components/NavBar/Other/LogOutCopmponents/MobileLogout/MobLogout.jsx
@@ -1,13 +1,10 @@
-import { useDispatch } from 'react-redux';
-import { logout } from 'redux/auth/auth-operations';
 import exit from '../../../../../images/user/LogOut.svg';
 import { MobLogOutBtn } from './MobLogout.styled';
 
-export const MobLogoutComponent = () => {
-  const dispatch = useDispatch();
+export const MobLogoutComponent = ({ openModal }) => {
 
   const handleLogout = () => {
-    dispatch(logout());
+    openModal();
   };
 
   return (
